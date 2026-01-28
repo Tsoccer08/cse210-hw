b570121f5baec6dcd89b07838953cd05c6e84be3
Troy Justesen <tjustesen08@gmail.com>
2026-01-27 17:39:22 -0700

    Abstraction

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -9,54 +9,47 @@ class Program
         string name = PromptUserName();
         int favoriteNumber = PromptUserNumber();
 
-        int birthYear;
-        PromptUserBirthYear(out birthYear);
+        PromptUserBirthYear(out int birthYear);
 
         int squaredNumber = SquareNumber(favoriteNumber);
 
         DisplayResult(name, squaredNumber, birthYear);
+    }
 
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
 
-            static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the program!");
-        }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
 
-        // Prompts for and returns the user's name
-        static string PromptUserName()
-        {
-            Console.Write("Please enter your name: ");
-            return Console.ReadLine();
-        }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
 
-        // Prompts for and returns the user's favorite number
-        static int PromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            return int.Parse(Console.ReadLine());
-        }
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write("Please enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
 
-        // Prompts for the user's birth year using an out parameter
-        static void PromptUserBirthYear(out int birthYear)
-        {
-            Console.Write("Please enter the year you were born: ");
-            birthYear = int.Parse(Console.ReadLine());
-        }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
 
-        // Returns the square of a number
-        static int SquareNumber(int number)
-        {
-            return number * number;
-        }
+    static void DisplayResult(string name, int squaredNumber, int birthYear)
+    {
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
 
-        // Displays the final results
-        static void DisplayResult(string name, int squaredNumber, int birthYear)
-        {
-            int currentYear = DateTime.Now.Year;
-            int age = currentYear - birthYear;
-
-            Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
-            Console.WriteLine($"{name}, you will turn {age} this year.");
-        }
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        Console.WriteLine($"{name}, you will turn {age} this year.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,96 +1,121 @@
 using System;
 using System.Reflection.Metadata;
+namespace Sandbox
+{
+    // string name = "";
 
-// string name = "";
+    // void DisplayMessage(string name)
+    //     {
+    //         Console.WriteLine($"Hello {name}");
+    //     }
 
-// void DisplayMessage(string name)
-//     {
-//         Console.WriteLine($"Hello {name}");
-//     }
+    // DisplayMessage(name);
 
-// DisplayMessage(name);
+    // Console.WriteLine("Unit Converter Calculator\n");
+    // Console.Write("Enter Inches to convert to Centimeters- ");
+    // string answer = Console.ReadLine();
+    // double inches = double.Parse(answer);
+    // double centimeters = inches * 2.54;
+    // Console.WriteLine($"there are {centimeters} centimeters");
 
-// Console.WriteLine("Unit Converter Calculator\n");
-// Console.Write("Enter Inches to convert to Centimeters- ");
-// string answer = Console.ReadLine();
-// double inches = double.Parse(answer);
-// double centimeters = inches * 2.54;
-// Console.WriteLine($"there are {centimeters} centimeters");
+    // int number = 5;
 
-// int number = 5;
+    // if (number > 10 || number < -10)
+    // {
+    //     Console.WriteLine($"your number ({number}) single multi-digit");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("Single Digit");
+    // }
 
-// if (number > 10 || number < -10)
-// {
-//     Console.WriteLine($"your number ({number}) single multi-digit");
-// }
-// else
-// {
-//     Console.WriteLine("Single Digit");
-// }
+    // string response = "yes";
 
-// string response = "yes";
+    // while (response == "yes")
+    // {
+    //     Console.Write("Do you want to continue? ");
+    //     response = Console.ReadLine();
+    // }
 
-// while (response == "yes")
-// {
-//     Console.Write("Do you want to continue? ");
-//     response = Console.ReadLine();
-// }
+    // Console.WriteLine("Done!");
 
-// Console.WriteLine("Done!");
+    // string rresponse;
 
-// string rresponse;
+    // do
+    // {
+    //     Console.Write("Do you want to continue? ");
+    //     rresponse = Console.ReadLine();
+    // }
+    // while (rresponse == "yes");
 
-// do
-// {
-//     Console.Write("Do you want to continue? ");
-//     rresponse = Console.ReadLine();
-// }
-// while (rresponse == "yes");
+    // Console.WriteLine("Done again!");
 
-// Console.WriteLine("Done again!");
+    // List<string> words = new List<string>();
+    // words.Add("cold");
+    // words.Add("snow");
+    // words.Add("soccer");
 
-// List<string> words = new List<string>();
-// words.Add("cold");
-// words.Add("snow");
-// words.Add("soccer");
+    // foreach (string word in words)
+    // {
+    //     Console.WriteLine(word);
+    // }
 
-// foreach (string word in words)
-// {
-//     Console.WriteLine(word);
-// }
+    // or int i = 0; outside for
+    // for(int i = 0; i < 10; i++)
+    // {
+    //     Console.WriteLine(i);
+    //     // or i++; inside loop
+    // }
 
-// or int i = 0; outside for
-// for(int i = 0; i < 10; i++)
-// {
-//     Console.WriteLine(i);
-//     // or i++; inside loop
-// }
+    // for(int i = 10; i > 0; i--)
+    // {
+    //     Console.WriteLine(i);
+    // }
 
-// for(int i = 10; i > 0; i--)
-// {
-//     Console.WriteLine(i);
-// }
+    // or for (int i = words.Count - 1; i >= 0; i--)
+    // for (int i = 0; i < words.Count; i++)
+    // {
+    //     Console.WriteLine(words[i]);
+    // }
 
-// or for (int i = words.Count - 1; i >= 0; i--)
-// for (int i = 0; i < words.Count; i++)
-// {
-//     Console.WriteLine(words[i]);
-// }
+    // comment, don't need one at end.
+    /* comment */
+    // to comment out many lines, select them, then do ctrl + /.
+    // you don't need brackets for if statements if it's only one line
+    // int number = -8;
+    // if (number < 10 && number > -10)
+    //      Console.WriteLine($"your number ({number}) single digit");
 
-// comment, don't need one at end.
-/* comment */
-// to comment out many lines, select them, then do ctrl + /.
-// you don't need brackets for if statements if it's only one line
-// int number = -8;
-// if (number < 10 && number > -10)
-//      Console.WriteLine($"your number ({number}) single digit");
+    // Console.Write("Give me a number: ");
+    // string user = Console.ReadLine();
+    // int number = int.Parse(user);
 
-// Console.Write("Give me a number: ");
-// string user = Console.ReadLine();
-// int number = int.Parse(user);
+    // for (int i = 1; i<=12; i++)
+    // {
+    //     Console.WriteLine($"{number * i}");
+    // }
 
-// for (int i = 1; i<=12; i++)
-// {
-//     Console.WriteLine($"{number * i}");
-// }
+    public class Program
+    {
+        public static void Main()
+        {
+            // Console.WriteLine("Hello World!");
 
+            // Counter myCounter = new Counter();
+            // myCounter.Run();
+
+            Message m1 = new Message();
+            m1._message = "Send help!";
+            m1._priority = 1;
+
+            Message m2 = new Message();
+            m2._message = "Please pick up some carrots at the store.";
+            m2._priority = 3;
+            
+            Console.WriteLine(m2.GetMessage());
+            Console.WriteLine(m1.GetMessage());
+
+            
+        }
+    }
+}
