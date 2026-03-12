5b3ed276f2e360f0e1d81585f8b4fa3c3764c4f1
Troy Justesen <tjustesen08@gmail.com>
2026-03-12 12:59:49 -0600

    fix

--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -2,6 +2,7 @@ public class Rectangle : Shape
 {
     private double _length;
     private double _width;
+    
 
     public Rectangle(string color, double length, double width) : base(color)
     {
